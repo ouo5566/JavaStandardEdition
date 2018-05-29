2c256b3558580ae280eed8f7e963322a9ccdd58c
ouo5566 <ouo5566@gmail.com>
2018-05-29 16:00:21 +0900

    폴더 정리

--- branchDemo/RPSGame.java
+++ branchDemo/RPSGame.java
@@ -1,29 +1,18 @@
 import java.util.Scanner;
-public class RPSGame{
+public class RPSGame02{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
-		System.out.println("rock, scissors, paper !");
-		System.out.println(" A : ? ");
-		String a = scan.next();
-		System.out.println(" B : ? ");
-		String b = scan.next();
+		System.out.println("A:(Rock_1,Scissors_2,Paper_3)? Number");
+		int a = scan.nextInt();
+		System.out.println("B:Rock_1,Scissors_2,Paper_3? Number");
+		int b = scan.nextInt();
 
-		String result = "";
-		if(
-			(a.equals("scissors") && b.equals("paper"))
-			|| (a.equals("rock") && b.equals("scissors"))
-			|| (a.equals("paper") && b.equals("rock"))
-			){
-			result = "A ½Â¸® !!";
-		}else if(
-			(a.equals("scissors") && b.equals("rock"))
-			|| (a.equals("rock") && b.equals("paper"))
-			|| (a.equals("paper") && b.equals("scissors"))
-			){
-			result = "B ½Â¸® !!";
-		}else{
-			result = "ºñ°å½À´Ï´Ù.";
+		String result = "A ½Â¸®";
+		if(a-b==0){
+			result = "ºñ±è";
+		}else if((a*2)/b!=1){
+			result = "B ½Â¸®";
 		}
-		System.out.println("[ A : " + a + ", B : " + b + " ] > " + result);
-		}
+		System.out.println(result);
+	}
 }
